9b05aae3bb6b35f8add1d8b303e2800c8b86d6ea
Ivan <v_cho@list.ru>
2025-12-25 13:42:01 +0300

    WAR convertion

--- src/main/java/com/github/irybov/bankdemomvc/BankDemoBootApplication.java
+++ src/main/java/com/github/irybov/bankdemomvc/BankDemoBootApplication.java
@@ -4,15 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@EnableWebMvc
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class,
 								ManagementWebSecurityAutoConfiguration.class})
-public class BankDemoBootApplication {
+public class BankDemoBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankDemoBootApplication.class, args);
 	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BankDemoBootApplication.class);
+    }
 }
